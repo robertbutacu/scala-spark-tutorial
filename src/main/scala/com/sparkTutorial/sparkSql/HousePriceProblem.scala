169089962072667563152b3c01e53a6a6e269b7f
robertbutacu <robert.butacu@yahoo.com>
2017-11-12 00:27:23 +0200

    HousePriceProblem()

--- src/main/scala/com/sparkTutorial/sparkSql/HousePriceProblem.scala
+++ src/main/scala/com/sparkTutorial/sparkSql/HousePriceProblem.scala
@@ -1,40 +1,65 @@
 package com.sparkTutorial.sparkSql
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
 
-object HousePriceProblem {
 
-        /* Create a Spark program to read the house data from in/RealEstate.csv,
-           group by location, aggregate the average price per SQ Ft and sort by average price per SQ Ft.
+object HousePriceProblem extends App{
 
-        The houses dataset contains a collection of recent real estate listings in San Luis Obispo county and
-        around it. 
+  /* Create a Spark program to read the house data from in/RealEstate.csv,
+     group by location, aggregate the average price per SQ Ft and sort by average price per SQ Ft.
 
-        The dataset contains the following fields:
-        1. MLS: Multiple listing service number for the house (unique ID).
-        2. Location: city/town where the house is located. Most locations are in San Luis Obispo county and
-        northern Santa Barbara county (Santa Maria­Orcutt, Lompoc, Guadelupe, Los Alamos), but there
-        some out of area locations as well.
-        3. Price: the most recent listing price of the house (in dollars).
-        4. Bedrooms: number of bedrooms.
-        5. Bathrooms: number of bathrooms.
-        6. Size: size of the house in square feet.
-        7. Price/SQ.ft: price of the house per square foot.
-        8. Status: type of sale. Thee types are represented in the dataset: Short Sale, Foreclosure and Regular.
+  The houses dataset contains a collection of recent real estate listings in San Luis Obispo county and
+  around it. 
 
-        Each field is comma separated.
+  The dataset contains the following fields:
+  1. MLS: Multiple listing service number for the house (unique ID).
+  2. Location: city/town where the house is located. Most locations are in San Luis Obispo county and
+  northern Santa Barbara county (Santa Maria­Orcutt, Lompoc, Guadelupe, Los Alamos), but there
+  some out of area locations as well.
+  3. Price: the most recent listing price of the house (in dollars).
+  4. Bedrooms: number of bedrooms.
+  5. Bathrooms: number of bathrooms.
+  6. Size: size of the house in square feet.
+  7. Price/SQ.ft: price of the house per square foot.
+  8. Status: type of sale. Thee types are represented in the dataset: Short Sale, Foreclosure and Regular.
 
-        Sample output:
+  Each field is comma separated.
 
-        +----------------+-----------------+
-        |        Location| avg(Price SQ Ft)|
-        +----------------+-----------------+
-        |          Oceano|             95.0|
-        |         Bradley|            206.0|
-        | San Luis Obispo|            359.0|
-        |      Santa Ynez|            491.4|
-        |         Cayucos|            887.0|
-        |................|.................|
-        |................|.................|
-        |................|.................|
-         */
+  Sample output:
+
+  +----------------+-----------------+
+  |        Location| avg(Price SQ Ft)|
+  +----------------+-----------------+
+  |          Oceano|             95.0|
+  |         Bradley|            206.0|
+  | San Luis Obispo|            359.0|
+  |      Santa Ynez|            491.4|
+  |         Cayucos|            887.0|
+  |................|.................|
+  |................|.................|
+  |................|.................|
+   */
+  val LOCATION = "Location"
+
+  val PRICE_SQ_FT = "Price SQ Ft"
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  val session = SparkSession.builder().appName("HousePricingProblem").master("local[1]").getOrCreate()
+
+  val dataFrameReader = session.read
+
+  val responses = dataFrameReader
+    .option("header", "true")
+    .option("inferSchema", value = true)
+    .csv("in/RealEstate.csv")
+  //group by location, aggregate the average price per SQ Ft and sort by average price per SQ Ft.
+
+  val housePricing = responses.select(LOCATION, PRICE_SQ_FT)
+
+  housePricing.groupBy(LOCATION)
+    .avg(PRICE_SQ_FT)
+    .orderBy("avg(Price SQ Ft)")
+    .show()
+
 }
